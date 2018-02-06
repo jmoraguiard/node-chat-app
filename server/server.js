55c0040a0a42e46eb57e473dc533f76095dcdf95
Joan Mora <joan@inflight-vr.com>
2018-02-06 16:41:21 +0100

    Add Socket.IO to the project

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
-// Use path library from node.js
+// Use path and http libraries from node.js
 const path = require('path');
+const http = require('http');
 // Use express library for developing web apps (remember to call npm i --save express)
 const express = require('express');
+// Load socket.io
+const socketIO = require('socket.io');
 
 // Create the path to the public folder
 const publicPath = path.join(__dirname + '/../public');
-
 // Set Heroku environment variables
-const port = process.env.Port || 3000;
-
+const port = process.env.PORT || 3000;
 // Create a express application variable
 var app = express();
+// create a server using the express app
+var server = http.createServer(app);
 
 // Make it serve by default public directory
 app.use(express.static(publicPath));
 
 // Configure it to listen to port 3000
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up and running in port ${port}.`);
 });
